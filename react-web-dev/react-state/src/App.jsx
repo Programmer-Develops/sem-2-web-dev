8bd90f810dd3881916cf9363b8b816fb2b834ecd
Programmer-Develops <shachipandya29@gmail.com>
2026-04-06 10:08:42 +0530

    implementing tailwindcss

--- react-web-dev/react-state/src/App.jsx
+++ react-web-dev/react-state/src/App.jsx
@@ -2,10 +2,10 @@ import './App.css'
 import Greet from './Components/Greet'
 
 function App() {
-
   return (
     <div className = "App">
       <h1 style={{ color: 'blue' }}>React</h1>
+      <p className={"text-center bg-orange-500 text-white"}>Tailwindcss</p>
       <Greet />
     </div>
   )
