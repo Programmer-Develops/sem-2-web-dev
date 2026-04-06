b57b0f8dd5acbced55dffa4d2d487f459faebf23
Programmer-Develops <shachipandya29@gmail.com>
2026-04-06 09:50:37 +0530

    learn module css

--- react-web-dev/react-state/src/App.jsx
+++ react-web-dev/react-state/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Greet from './Components/Greet'
 
 function App() {
 
   return (
     <div className = "App">
       <h1 style={{ color: 'blue' }}>React</h1>
+      <Greet />
     </div>
   )
 }
---
+++ react-web-dev/react-state/src/Components/Greet.jsx
@@ -0,0 +1,9 @@
+import styles from "./Greet.module.css"
+
+export default function Greet() {
+    return (
+        <h2 className = {styles.App}>
+            Let's do it!
+        </h2>
+    )
+}
