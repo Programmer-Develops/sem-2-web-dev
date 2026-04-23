a827441322b39c12622fb841ce5fad2d9c717359
Programmer-Develops <shachipandya29@gmail.com>
2026-04-23 09:18:38 +0530

    learning ContextAPI

---
+++ react-web-dev/react-context2/src/Components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+    return (
+        <div >
+            <h3>Home</h3>
+            <h3>About</h3>
+            <h3>Auth:</h3>
+            <h3>Login</h3>
+            <h3>Logout</h3>
+        </div>
+    )
+}
