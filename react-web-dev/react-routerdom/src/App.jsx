47e20de5617961311fe72e8f40e4578089ea61d4
Programmer-Develops <shachipandya29@gmail.com>
2026-04-23 10:17:20 +0530

    Add AllRoutes component with basic routing setup

--- react-web-dev/react-routerdom/src/App.jsx
+++ react-web-dev/react-routerdom/src/App.jsx
@@ -1,10 +1,12 @@
 import Nav from './Components/Nav';
+import AllRoutes from './Components/AllRoutes';
 
 export default function App() {
   return (
     <div>
       <h1>React Router Dom</h1>
       <Nav />
+      <AllRoutes />
     </div>
   )
 }
